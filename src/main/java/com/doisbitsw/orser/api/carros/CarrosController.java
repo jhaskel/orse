2286b56e89a2c1943af1291fc0caa304bff536d5
Joao Haskel <johaskel@gmail.com>
2021-02-24 08:53:14 -0300

    carros

--- src/main/java/com/doisbitsw/orser/api/carros/CarrosController.java
+++ src/main/java/com/doisbitsw/orser/api/carros/CarrosController.java
@@ -10,7 +10,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/carros")
+@RequestMapping("/api/v1/carro")
 public class CarrosController {
     @Autowired
     private CarroService service;
