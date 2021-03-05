2a31e948a5499ca94d578d6ed75d782936542c8f
Joao Haskel <johaskel@gmail.com>
2021-03-05 13:34:58 -0300

    protocolos - getAtivo

--- src/main/java/com/doisbitsw/orser/api/protocolos/ProtocolosRepository.java
+++ src/main/java/com/doisbitsw/orser/api/protocolos/ProtocolosRepository.java
@@ -10,7 +10,7 @@ public interface ProtocolosRepository extends JpaRepository<Protocolos, Long> {
     @Query(value = "SELECT * FROM protocolos  ORDER BY id ", nativeQuery = true)
     List<Protocolos> findAll();
 
-    @Query(value = "SELECT *  FROM protocolos where isativo = true an entidade = :entidade and ano =:ano order by id desc ", nativeQuery = true)
+    @Query(value = "SELECT *  FROM protocolos where isativo = true and entidade = :entidade and ano =:ano order by id desc ", nativeQuery = true)
     List<Protocolos> findAtivo(Long entidade, Long ano);
 
 
