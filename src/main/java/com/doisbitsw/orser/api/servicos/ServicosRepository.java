38e857c1a4f0ab5bef8b501643db01b9a150a276
Joao Haskel <johaskel@gmail.com>
2021-03-26 21:34:27 -0300

    veiculos - setor

--- src/main/java/com/doisbitsw/orser/api/servicos/ServicosRepository.java
+++ src/main/java/com/doisbitsw/orser/api/servicos/ServicosRepository.java
@@ -7,13 +7,13 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ServicosRepository extends JpaRepository<Servicos, Long> {
-    @Query(value = "SELECT ser.* FROM servicos ser  ORDER BY ser.id desc", nativeQuery = true)
+    @Query(value = "SELECT ser.*,count(ser.id) as quant, ent.cidade AS nomec FROM servicos ser  ORDER BY ser.id desc", nativeQuery = true)
     List<Servicos> findAll();
 
     @Query(value = "SELECT ser.*,count(ser.id) as quant, ent.cidade AS nomec FROM servicos ser\n" +
             "INNER JOIN entidade ent ON ent.id = ser.entidade\n" +
-            "where ser.entidade = 1 \n" +
-            "GROUP BY ser.id ", nativeQuery = true)
+            "where ser.entidade = :entidade\n" +
+            "GROUP BY ser.id ORDER BY ser.id desc ", nativeQuery = true)
     List<Servicos> findAlgo(Long entidade);
 
 
