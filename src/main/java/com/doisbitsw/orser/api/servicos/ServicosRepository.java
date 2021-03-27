336b534d4f07d926f2c5662010280b02e59ddb14
Joao Haskel <johaskel@gmail.com>
2021-03-26 21:15:21 -0300

    veiculos - setor

--- src/main/java/com/doisbitsw/orser/api/servicos/ServicosRepository.java
+++ src/main/java/com/doisbitsw/orser/api/servicos/ServicosRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface ServicosRepository extends JpaRepository<Servicos, Long> {
 
 
-    @Query(value = "SELECT ser.*,count(ser.id) as quant, ent.cidade AS nomeCidade FROM servicos ser\n" +
+    @Query(value = "SELECT *,count(ser.id) as quant, ent.cidade AS nomeCidade FROM servicos ser\n" +
             "INNER JOIN entidade ent ON ent.id = ser.entidade\n" +
             "where ser.entidade = 1 ", nativeQuery = true)
     List<Servicos> findAlgo(Long entidade);
