3f6244d5509db29230f1eccfbe41d97cd87203bd
Joao Haskel <johaskel@gmail.com>
2021-04-08 17:03:07 -0300

    Serviços - ispublic

--- src/main/java/com/doisbitsw/orser/api/maqProto/MaqProtoRepository.java
+++ src/main/java/com/doisbitsw/orser/api/maqProto/MaqProtoRepository.java
@@ -30,7 +30,7 @@ public interface MaqProtoRepository extends JpaRepository<MaqProto, Long> {
             "ORDER BY quant desc", nativeQuery = true)
     List<MaqProto> findRelatorio(Long setor,Long ano);
 
-    @Query(value = "SELECT maq.*, COUNT(maq.id) AS quant,vei.nome as nome FROM maq_proto maq\n" +
+    @Query(value = "SELECT maq.*, COUNT(maq.id) AS quant,vei.nome as nome,vei.identificador AS identificador FROM maq_proto maq\n" +
             "INNER JOIN protocolos pro ON pro.id = maq.protocolo\n" +
             "INNER JOIN veiculos vei ON vei.id = maq.maquina\n" +
             "WHERE pro.isagendado = TRUE and ano = ano AND vei.isveiculo = TRUE AND  pro.setor = :setor AND maq.maquina = :maquina\n" +
