5dda1793fb87c62818201aeff5e53ade602597a1
Jhaskel <johaskel@gmail.com>
2021-08-18 14:16:46 -0300

    maq

--- src/main/java/com/doisbitsw/orser/api/maqProto/MaqProtoRepository.java
+++ src/main/java/com/doisbitsw/orser/api/maqProto/MaqProtoRepository.java
@@ -28,7 +28,7 @@ public interface MaqProtoRepository extends JpaRepository<MaqProto, Long> {
             "GROUP BY maq.maquina", nativeQuery = true)
     List<MaqProto> findCod(String cod);
 
-    @Query(value = "SELECT maq.*,COUNT(maq.id) AS quant,vei.nome as nome ,vei.nome AS identificador,vei.nome as operador,vei.nome as imple\n" +
+    @Query(value = "SELECT maq.*,COUNT(maq.id) AS quant,vei.nome as nome ,vei.identificador AS identificador,vei.nome as operador,vei.nome as imple\n" +
             "FROM maq_proto maq\n" +
             "INNER JOIN veiculos vei ON vei.id = maq.maquina          \n" +
             "where vei.entidade = :entidade\n" +
