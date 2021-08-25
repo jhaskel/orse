0a71978d495b52c52d61b8aa3cb7d37a47f08b47
Jhaskel <johaskel@gmail.com>
2021-08-25 11:34:09 -0300

    maq

--- src/main/java/com/doisbitsw/orser/api/veiculos/VeiculosController.java
+++ src/main/java/com/doisbitsw/orser/api/veiculos/VeiculosController.java
@@ -52,9 +52,9 @@ public class VeiculosController {
                 ResponseEntity.noContent().build() :
                 ResponseEntity.ok(carros);
     }
-    @GetMapping("/setorAgendado/{setor}")
-    public ResponseEntity getSetorAgendado(@PathVariable("setor") Long setor) {
-        List<VeiculosDTO> carros = service.getSetorAgendado(setor);
+    @GetMapping("/setorAgendado/{setor}/{operador}")
+    public ResponseEntity getSetorAgendado(@PathVariable("setor") Long setor,@PathVariable("operador") String operador) {
+        List<VeiculosDTO> carros = service.getSetorAgendado(setor,operador);
         return carros.isEmpty() ?
                 ResponseEntity.noContent().build() :
                 ResponseEntity.ok(carros);
--- src/main/java/com/doisbitsw/orser/api/veiculos/VeiculosRepository.java
+++ src/main/java/com/doisbitsw/orser/api/veiculos/VeiculosRepository.java
@@ -15,9 +15,15 @@ public interface VeiculosRepository extends JpaRepository<Veiculos, Long> {
     @Query(value = "SELECT * FROM veiculos where entidade = :entidade and setor = :setor  ORDER BY isativo desc,isdisponivel desc,id desc", nativeQuery = true)
     List<Veiculos> findEntidadeSetor(Long entidade,Long setor);
 
-    @Query(value = "select * from veiculos vei\n" +
+    @Query(value = "select vei.* from veiculos vei\n" +
+            "left join maq_proto maq on maq.maquina = vei.id \n" +
+            "where vei.setor = :setor and vei.isdisponivel = true and vei.isativo = true and vei.id NOT IN (SELECT maquina FROM maq_proto where protocolo = :operador)\n" +
+            "group by vei.id", nativeQuery = true)
+    List<Veiculos> findSetorAgendado(Long setor,String operador);
+
+    /*@Query(value = "select * from veiculos vei\n" +
             "where vei.setor = :setor  and vei.isativo = true and vei.isdisponivel = true ", nativeQuery = true)
-    List<Veiculos> findSetorAgendado(Long setor);
+    List<Veiculos> findSetorAgendado(Long setor);*/
 
     /*@Query(value = "select * from veiculos vei\n" +
             "where vei.setor = :setor  and vei.isativo = true and vei.isdisponivel = true and  vei.id NOT IN (SELECT maquina FROM maq_proto where cod = :operador)", nativeQuery = true)
